9f790c86d4104aaae7f24133d0c6e1b2f1ed1992
HarisAhmed777 <ahmedharis371@gmail.com>
2024-12-13 22:36:49 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "b2140026c8mshfd3bd23352c062fp1aad78jsne3fb12394926",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "c602a008c7f137a836a2f28def90cf4a"; // enter your key from openweather API
